b4da7252f254b831fb4bc3d9b416ed0ed5c80dc4
shahirov <shahirov.ara@gmail.com>
2020-10-21 13:37:22 +0500

    run firebase initialization

---
+++ src/lib/firebase/index.ts
@@ -0,0 +1,34 @@
+/* eslint-disable import/no-duplicates */
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import firebase from 'firebase/app'
+
+type Config = {
+  apiKey: string
+  authDomain: string
+  databaseURL: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId: string
+  measurementId: string
+}
+
+const config: Partial<Config> = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+}
+
+firebase.initializeApp(config)
+
+export const firestore = firebase.firestore()
+export const auth = firebase.auth()
+
+// eslint-disable-next-line import/no-default-export
+export default firebase
