7b86f5c148bca48394320a29b8ef43c4757405bf
shahirov <shahirov.ara@gmail.com>
2020-11-05 16:12:52 +0500

    fix bug with theme

--- src/features/theme/init.ts
+++ src/features/theme/init.ts
@@ -3,13 +3,8 @@ import { forward } from 'effector'
 import { $theme, loadThemeFx, saveThemeFx, toggleTheme } from './model'
 
 $theme
-  .on([loadThemeFx.doneData], (_, theme) => theme)
-  .on(toggleTheme, (_, event) => {
-    const { checked } = event.currentTarget
-
-    if (checked) return 'dark'
-    return 'light'
-  })
+  .on(loadThemeFx.doneData, (_, theme) => theme)
+  .on(toggleTheme, (theme) => (theme === 'light' ? 'dark' : 'light'))
 
 forward({
   from: $theme,
