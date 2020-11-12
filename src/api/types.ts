f54ada868d87504892bbe0fee4f18e723bb2ad1c
shahirov <shahirov.ara@gmail.com>
2020-11-12 16:04:59 +0500

    add weather api functions

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,4 +1,9 @@
-export type UserDocumentData = {
+export type Params = URLSearchParams | string | { [key: string]: string }
+
+export type UnitMeasurement = 'metric' | 'imperial'
+
+export type User = {
+  id: string
   email: string
   photoUrl: string
   createdAt: Date
@@ -18,4 +23,102 @@ export type City = {
   regionCode: string
   type: string
   wikiDataId: string
+}
+
+export type WeatherData = {
+  coord: {
+    lon: number
+    lat: number
+  }
+  weather: Array<{
+    id: number
+    main: string
+    description: string
+    icon: string
+  }>
+  base: string
+  main: {
+    temp: number
+    feels_like: number
+    temp_min: number
+    temp_max: number
+    pressure: number
+    humidity: number
+  }
+  visibility: number
+  wind: {
+    speed: number
+    deg: number
+  }
+  clouds: {
+    all: number
+  }
+  dt: number
+  sys: {
+    type: number
+    id: number
+    message: number
+    country: string
+    sunrise: number
+    sunset: number
+  }
+  timezone: number
+  id: number
+  name: string
+  cod: number
+}
+
+export type ForecastData = {
+  cod: string
+  message: number
+  cnt: number
+  list: Array<{
+    dt: number
+    main: {
+      temp: number
+      feels_like: number
+      temp_min: number
+      temp_max: number
+      pressure: number
+      sea_level: number
+      grnd_level: number
+      humidity: number
+      temp_kf: number
+    }
+    weather: Array<{
+      id: number
+      main: string
+      description: string
+      icon: string
+    }>
+    clouds: {
+      all: number
+    }
+    wind: {
+      speed: number
+      deg: number
+    }
+    visibility: number
+    pop: number
+    sys: {
+      pod: string
+    }
+    dt_txt: string
+    rain: {
+      '3h': number
+    }
+  }>
+  city: {
+    id: number
+    name: string
+    coord: {
+      lat: number
+      lon: number
+    }
+    country: string
+    population: number
+    timezone: number
+    sunrise: number
+    sunset: number
+  }
 }
---
+++ src/api/weather.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import { createEffect } from 'effector'
+
+import { ForecastData, Params, UnitMeasurement, WeatherData } from './types'
+
+const API_ENDPOINT = 'https://api.openweathermap.org/data/2.5'
+
+const request = axios.create({
+  baseURL: API_ENDPOINT,
+  paramsSerializer: (params: Params) => {
+    const qs = String(new URLSearchParams(params))
+    return qs && `${qs}&appid=${process.env.OPEN_WEATHER_API_KEY ?? ''}`
+  },
+})
+
+export const getWeatherDataByCityNameFx = createEffect<
+  {
+    cityName: string
+    units?: UnitMeasurement
+  },
+  AxiosResponse<WeatherData>,
+  AxiosError
+>(({ cityName, units = 'metric' }) =>
+  request.get('weather', {
+    params: {
+      q: cityName,
+      units,
+    },
+  }),
+)
+
+export const getForecastDataByCityNameFx = createEffect<
+  {
+    cityName: string
+    units?: UnitMeasurement
+  },
+  AxiosResponse<ForecastData>,
+  AxiosError
+>(({ cityName, units = 'metric' }) =>
+  request.get('forecast', {
+    params: {
+      q: cityName,
+      units,
+    },
+  }),
+)
