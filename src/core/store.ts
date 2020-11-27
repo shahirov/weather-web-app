4f36e60d98b70679e989eded4758b247f6b3d401
shahirov <shahirov.ara@gmail.com>
2020-11-27 23:30:48 +0500

    add store and root reducer with reducer hmr

---
+++ src/core/store.ts
@@ -0,0 +1,18 @@
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit'
+
+import { rootReducer, RootState } from './root-reducer'
+
+export const store = configureStore({
+  reducer: rootReducer,
+})
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./root-reducer', () => {
+    // eslint-disable-next-line global-require
+    const newRootReducer = require('./root-reducer').default
+    store.replaceReducer(newRootReducer)
+  })
+}
+
+export type AppDispatch = typeof store.dispatch
+export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>
